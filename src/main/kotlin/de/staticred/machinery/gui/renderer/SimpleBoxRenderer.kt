80df5e2237e4742791a450d0ed916fb3ad01ee6e
devinfritz <devinfritz@gmx.de>
2022-09-22 16:39:41 +0200

    Removed box renderer

--- src/main/kotlin/de/staticred/machinery/gui/renderer/SimpleBoxRenderer.kt
+++ src/main/kotlin/de/staticred/machinery/gui/renderer/SimpleBoxRenderer.kt
@@ -6,69 +6,22 @@ import javafx.scene.text.Text
 import javafx.scene.text.TextAlignment
 import javafx.scene.text.TextFlow
 import tornadofx.add
+import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 class SimpleBoxRenderer(private val textFlow: TextFlow): Renderer<Box> {
 
     override fun render(t: Box) {
         textFlow.children.clear()
+        val test = Text("""
+        ╭───╮
+        │   ╳   │
+        ╰───╯
+        """.trimIndent())
 
-        if (t.width > 46) error("Box cant be wider than 46 units")
+        test.fill = Color.ORANGE
 
-        val widthText = Text()
-        widthText.fill = Color.WHITE
-
-        widthText.text += "╔"
-        for (i in 0 until t.width) {
-            widthText.text += ("═")
-        }
-
-
-        widthText.text += "╗\n"
-        textFlow.children.add(widthText)
-
-        for (i in 0 until t.height / 2) {
-            val heightText = Text().apply { fill = Color.WHITE }
-            heightText.text += "║"
-
-            textFlow.children.add(heightText)
-
-
-
-            val filler = Text()
-            for (j in 0 until t.width) {
-                filler.text += "═"
-            }
-            textFlow.children.add(filler)
-            val heightTextSec = Text().apply { fill = Color.WHITE }
-            heightTextSec.text += "║\n"
-            textFlow.children.add(heightTextSec)
-        }
-
-        val widthTextBottom = Text()
-        widthTextBottom.fill = Color.WHITE
-
-        widthTextBottom.text += "╚"
-        for (i in 0 until t.width) {
-            widthTextBottom.text += "═"
-        }
-        widthTextBottom.text += "╝"
-        textFlow.children.add(widthTextBottom)
-
-
-    }
-
-    private fun getPixelWidthForFiller(fillerWidth: Int): Double {
-        return getPixelWidthOf("═".repeat(fillerWidth))
-    }
-
-    private fun getPixelWidthOf(string: String): Double {
-        val text = Text(string)
-        return text.layoutBounds.width
-    }
-
-    private fun getPixelHeightOf(string: String): Double {
-        val text = Text(string)
-        return text.layoutBounds.height
+        textFlow.children.add(test)
     }
 
 }
